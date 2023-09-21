654cf8a728bcc6c8a1a3f9ba735f1290eed77d56
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-09-21 19:09:08 +0200

    Update formulario.component.ts

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent {
+  public user!: any;
 
+  constructor() {
+    this.user = {
+      nombre: '',
+      apellidos: '',
+      bio: '',
+      genero: '',
+    };
+  }
+
+  ngOnInit() {}
 }
